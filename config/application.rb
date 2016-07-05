65c5aca94448be50eb86c6d9f77f4c577ec83056
niclin <bboyceo@hotmail.com>
2016-07-05 14:46:39 +0800

    Update app config to pass generate assets and helper

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,7 @@ module Rspec101
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.generators.assets = false
+    config.generators.helper = false
   end
 end
