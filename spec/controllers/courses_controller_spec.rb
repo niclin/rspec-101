ac33e8d1bbeb088d4a44753780ce4c13b2f29d32
niclin <bboyceo@hotmail.com>
2016-07-05 15:05:29 +0800

    Rename controller course to courses

---
+++ spec/controllers/courses_controller_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe CoursesController, type: :controller do
+
+
+  describe "GET index" do
+    it "assigns @courses and render template" do
+      course1 = Course.create(title: "foo", description: "bar")
+      course2 = Course.create(title: "bar", description: "foo")
+
+      get:index
+
+      expect(assigns[:courses].to eq([course1, course2]))
+      expect(response).to render_template("index")
+    end
+  end
+
+
+end
