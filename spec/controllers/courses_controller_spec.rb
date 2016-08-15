d19252ec65792bb4ae508eb398be239de172c3c2
niclin <bboyceo@hotmail.com>
2016-08-15 17:27:42 +0800

    One assertion per test

--- spec/controllers/courses_controller_spec.rb
+++ spec/controllers/courses_controller_spec.rb
@@ -7,10 +7,14 @@ RSpec.describe CoursesController, type: :controller do
     it "assigns @courses and render template" do
       course1 = FactoryGirl.create(:course)
       course2 = FactoryGirl.create(:course)
-
       get:index
-
       expect(assigns[:courses]).to eq([course1, course2])
+    end
+
+    it "render template" do
+      course1 = FactoryGirl.create(:course)
+      course2 = FactoryGirl.create(:course)
+      get:index
       expect(response).to render_template("index")
     end
   end
