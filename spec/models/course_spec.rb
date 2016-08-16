a4aa13ceaea7eaba266609819418ea72d0d903f6
niclin <bboyceo@hotmail.com>
2016-08-16 14:50:19 +0800

    Fix bug

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe Course, type: :model do
-  it { is_expected.to validate_presence_ot(:title)}
+  it { is_expected.to validate_presence_of(:title)}
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,6 +17,7 @@
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
+
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
@@ -39,7 +40,6 @@ RSpec.configure do |config|
     # `true` in RSpec 4.
     mocks.verify_partial_doubles = true
   end
-
   # The settings below are suggested to provide a good initial experience
   # with RSpec, but feel free to customize to your heart's content.
 =begin
