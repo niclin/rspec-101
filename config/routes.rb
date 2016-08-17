e0a257c015c6d684d869d82258b848101cc551b4
niclin <bboyceo@hotmail.com>
2016-08-17 14:05:18 +0800

    Add bootstrap sass and devise gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  devise_for :users
   resources :courses
   root "courses#index"
   # The priority is based upon order of creation: first created -> highest priority.
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,4 +1,7 @@
-FactoryGirl.define do
+FactoryGirl.define do  factory :user do
+    
+  end
+
   factory :course do
     description "Description"
     title "Course Name"
